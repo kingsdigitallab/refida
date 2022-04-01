14c5f993eb66bb2c1cf86a127cf646b883341b05
Miguel Vieira <jmvieira@pm.me>
2022-04-01 16:39:27 +0100

    👌 Improve: Add functions for entities data.

--- refida/data.py
+++ refida/data.py
@@ -1,17 +1,19 @@
 import ast
+import pickle
 from pathlib import Path
 from typing import Iterator, Optional
 
 import pandas as pd
+from functools import lru_cache
 
 from settings import DATA_DIR
 
 
-def get_raw_data(datadir: str = DATA_DIR) -> Iterator[Path]:
+def get_raw_data(datadir: str = DATA_DIR.name) -> Iterator[Path]:
     return get_raw_data_path(datadir).glob("**/*.pdf")
 
 
-def get_raw_data_path(datadir: str = DATA_DIR) -> Path:
+def get_raw_data_path(datadir: str = DATA_DIR.name) -> Path:
     return get_data_path(datadir, "0_raw")
 
 
@@ -42,7 +44,7 @@ def get_data_path(
     return path
 
 
-def get_etl_data(datadir: str = DATA_DIR) -> Optional[pd.DataFrame]:
+def get_etl_data(datadir: str = DATA_DIR.name) -> Optional[pd.DataFrame]:
     return get_data(get_etl_data_path(datadir))
 
 
@@ -53,21 +55,50 @@ def get_data(filename: Path, kwargs: dict = {}) -> Optional[pd.DataFrame]:
         return None
 
 
-def get_etl_data_path(datadir: str = DATA_DIR) -> Path:
+def get_etl_data_path(datadir: str = DATA_DIR.name) -> Path:
     return get_data_path(datadir, "1_interim", "etl.csv")
 
 
-def get_topics_data(datadir: str = DATA_DIR) -> Optional[pd.DataFrame]:
+def get_topics_data(datadir: str = DATA_DIR.name) -> Optional[pd.DataFrame]:
     return get_data(get_topics_data_path(datadir))
 
 
-def get_topics_data_path(datadir: str = DATA_DIR) -> Path:
+def get_topics_data_path(datadir: str = DATA_DIR.name) -> Path:
     return get_data_path(datadir, "1_interim", "topics.csv")
 
 
-def get_summaries_data(datadir: str = DATA_DIR) -> Optional[pd.DataFrame]:
+def get_summaries_data(datadir: str = DATA_DIR.name) -> Optional[pd.DataFrame]:
     return get_data(get_summaries_data_path(datadir))
 
 
-def get_summaries_data_path(datadir: str = DATA_DIR) -> Path:
+def get_summaries_data_path(datadir: str = DATA_DIR.name) -> Path:
     return get_data_path(datadir, "1_interim", "summaries.csv")
+
+
+def get_entities_data(
+    label: str, datadir: str = DATA_DIR.name
+) -> Optional[pd.DataFrame]:
+    return get_data(
+        get_entities_data_path(label, datadir),
+        # dict(converters=dict(doc=ast.literal_eval)),
+    )
+
+
+def get_entities_data_path(label: str, datadir: str = DATA_DIR.name) -> Path:
+    return get_data_path(datadir, "1_interim", f"entities_{label}.csv")
+
+
+@lru_cache(maxsize=512)
+def get_spacy_doc(
+    label: str, idx: int, datadir: str = DATA_DIR.name
+) -> Optional[pd.DataFrame]:
+    return get_spacy_docs(label, datadir)[idx]
+
+
+def get_spacy_docs(label: str, datadir: str = DATA_DIR.name) -> Optional[pd.DataFrame]:
+    with open(get_spacy_docs_path(label, datadir), "rb") as f:
+        return pickle.load(f)
+
+
+def get_spacy_docs_path(label: str, datadir: str = DATA_DIR.name) -> Path:
+    return get_data_path(datadir, "1_interim", f"spacy_docs_{label}.csv")
