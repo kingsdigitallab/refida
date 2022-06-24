2ae974ca0e59034137337dd226323489b66a20eb
Miguel Vieira <jmvieira@pm.me>
2022-06-24 07:30:59 +0100

    👌 Improve: Increase default topics threshold to 0.75.

--- settings.py
+++ settings.py
@@ -65,7 +65,7 @@ FEATURE_GEO_PLACE_LAT = "place_lat"
 FEATURE_GEO_PLACE_LON = "place_lon"
 FEATURE_GEO_GEOJSON = "geojson"
 
-DEFAULT_FILTER_TOPICS_SCORE_THRESHOLD = 0.5
+DEFAULT_FILTER_TOPICS_SCORE_THRESHOLD = 0.75
 
 # =====================================================================================
 # search module settings
